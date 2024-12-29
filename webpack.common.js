c5c7db4bbe386cae1479808e152354788c05d89b
lander-vitug-digy4 <lander.vitug@digy4.com>
2024-12-29 13:42:26 -0600

    Change project name to Dropdown

--- webpack.common.js
+++ webpack.common.js
@@ -33,7 +33,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: "Webpack Template",
+      title: "Dropdown Menu",
       template: "./src/template.html",
     }),
   ],
