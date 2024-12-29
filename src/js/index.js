b4a5e6fb5fb57f0d92c4735ce0ccb246e084c31e
lander-vitug-digy4 <lander.vitug@digy4.com>
2024-12-29 14:53:13 -0600

    Format changes

--- src/js/index.js
+++ src/js/index.js
@@ -1,21 +1,22 @@
 function initDropdown() {
-    const dropdowns = document.querySelectorAll('.dropdown');
-    
-    function toggleDropdown() {
-        const content = this.querySelector('.dropdown-content');
-        content.style.display = content.style.display === 'block'? 'none' : 'block';
-    }
+  const dropdowns = document.querySelectorAll(".dropdown");
 
-    dropdowns.forEach(dropdown => {
-        dropdown.addEventListener('click', toggleDropdown);
-        const content = dropdown.querySelector('.dropdown-content');
-        content.style.display = 'none';
-    });
+  function toggleDropdown() {
+    const content = this.querySelector(".dropdown-content");
+    content.style.display =
+      content.style.display === "block" ? "none" : "block";
+  }
+
+  dropdowns.forEach((dropdown) => {
+    dropdown.addEventListener("click", toggleDropdown);
+    const content = dropdown.querySelector(".dropdown-content");
+    content.style.display = "none";
+  });
 }
 
 (() => {
-    window.addEventListener('DOMContentLoaded', () => {
-        // add button event handler
-        initDropdown();
-    });
+  window.addEventListener("DOMContentLoaded", () => {
+    // add button event handler
+    initDropdown();
+  });
 })();
