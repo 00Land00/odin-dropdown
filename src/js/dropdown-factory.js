6b246539d19e0cddc64764a990444b698847741e
lander-vitug-digy4 <lander.vitug@digy4.com>
2025-01-01 19:05:50 -0600

    format

--- src/js/dropdown-factory.js
+++ src/js/dropdown-factory.js
@@ -1,30 +1,30 @@
 import "styles/styles.css";
 
 function createDropdownElement(title, items) {
-    const dropdownElement = document.createElement("div");
-    dropdownElement.classList.add("dropdown");
+  const dropdownElement = document.createElement("div");
+  dropdownElement.classList.add("dropdown");
 
-    const dropdownContent = document.createElement("div");
-    dropdownContent.classList.add("dropdown-content");
-    dropdownContent.classList.add("hidden");
-    items.forEach(item => {
-        const dropdownItem = document.createElement("div");
-        dropdownItem.innerHTML = item.name;
-        dropdownItem.addEventListener("click", item.callbackFn);
-        dropdownContent.appendChild(dropdownItem);
-    });
+  const dropdownContent = document.createElement("div");
+  dropdownContent.classList.add("dropdown-content");
+  dropdownContent.classList.add("hidden");
+  items.forEach((item) => {
+    const dropdownItem = document.createElement("div");
+    dropdownItem.innerHTML = item.name;
+    dropdownItem.addEventListener("click", item.callbackFn);
+    dropdownContent.appendChild(dropdownItem);
+  });
 
-    const dropdownButton = document.createElement("button");
-    dropdownButton.classList.add("dropdown-btn");
-    dropdownButton.innerHTML = title;
-    dropdownButton.addEventListener("click", () => {
-        dropdownContent.classList.toggle("hidden");
-    });
+  const dropdownButton = document.createElement("button");
+  dropdownButton.classList.add("dropdown-btn");
+  dropdownButton.innerHTML = title;
+  dropdownButton.addEventListener("click", () => {
+    dropdownContent.classList.toggle("hidden");
+  });
 
-    dropdownElement.appendChild(dropdownButton);
-    dropdownElement.appendChild(dropdownContent);
+  dropdownElement.appendChild(dropdownButton);
+  dropdownElement.appendChild(dropdownContent);
 
-    return dropdownElement;
+  return dropdownElement;
 }
 
 export default createDropdownElement;
